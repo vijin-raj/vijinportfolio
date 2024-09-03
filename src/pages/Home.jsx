7a3a592e461bd7fe87f54f405d789c370c475e74
vijincapecom <142965599+vijincapecom@users.noreply.github.com>
2024-09-03 18:02:19 +0530

    latest

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import { Grid, Stack, Typography, Container, Box, useTheme } from '@mui/material'
 import React from 'react'
-import { avatarCircle, handyhifi, online } from '../images/images'
+import { avatarCircle, avtwo, handyhifi, online } from '../images/images'
 import FmdGoodOutlinedIcon from '@mui/icons-material/FmdGoodOutlined';
 import { Toolbarstyle } from '../theme/helpertheme';
 
@@ -9,7 +9,7 @@ const Home = () => {
   return (
     <Box component={"main"} >
       <Toolbarstyle />
-    <Container maxWidth="lg" sx={{marginTop: {xs: 8 ,sm: 10 , md:10}, marginBottom: {xs: 1 ,sm: 2 , md:3}} } >
+    <Container maxWidth="lg" sx={{marginTop: {xs: 8 ,sm: 6 , md:10}, marginBottom: {xs: 1 ,sm: 2 , md:1}} } >
       <div className='home-div-home' >
 
         <Grid container spacing={2} sx={{
@@ -20,16 +20,17 @@ const Home = () => {
         }}>
 
           <Grid item xs={12} md={9} pl={2}>
-              <Typography variant='h1' mt={{ xs: 2, md: 0 }} className='h1-font-smallscreen'>Hi, I'm Vijin Raj
+              <Typography variant='h1'  className='h1-font-smallscreen'>Hi, I'm Vijin Raj
               <img src={handyhifi} alt='wave'  className='wave-imageone' />
               </Typography>
 
             <div className='home-div-content'>
               <Box>
                 <Typography variant='h6' mt={2} textAlign={{ xs: "flex-start", md: "unset" }} pl={0.5}>
-                  I'm a Frond End developer (React.js & Node.js) with a focus on creating
-                  (and occasionally designing) exceptional digital experiences that are fast,
-                  accessible, visually appealing, and responsive. Even though I have been creating web applications for over 7 years, I still love it as if it was something new.
+                I'm a Front-End Developer with 2 years of experience specializing in React.js. I focus on creating exceptional
+                 digital experiences that are fast, accessible, visually appealing, and responsive. 
+                Despite being relatively new to the field, my passion for web development drives me 
+                to continuously learn and take on new challenges.
                 </Typography>
               </Box>
             </div>
@@ -37,7 +38,7 @@ const Home = () => {
               <Stack direction={"row"} gap={1} alignItems={"center"} sx={{ display: { xs: "flex", md: "flex" }, justifyContent: { xs: "flex-start", md: "unset" } }}  >
                 <FmdGoodOutlinedIcon width={7} height={10} sx={{color: theme?.palette?.mode  === "dark" && "#FFFFFF" }} />
                 <Typography variant='h6'  >
-                  Tharkuripatti
+                  Nagercoil, TN
                 </Typography>
               </Stack>
 
@@ -50,8 +51,8 @@ const Home = () => {
             </div>
 
           </Grid>
-          <Grid item xs={12} md={3} display={"flex"} justifyContent={{ xs: "center", md: "flex-end" }}>
-            <img src={avatarCircle} alt='avatarcircle' className='avatarcircle-img' />
+          <Grid item xs={12} md={3} display={"flex"} justifyContent={{ xs: "center", md: "flex-end" }} >
+              <img src={avatarCircle} alt='avatarcircle' className='avatarcircle-img' /> 
           </Grid>
 
         </Grid>
